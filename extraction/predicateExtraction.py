4aebb4d41fc41a4a0257c9c51d2a6d81fc4fcc52
imjuliayue <juliayue6@gmail.com>
2025-08-01 17:28:19 -0400

    Added extraction functions

--- extraction/predicateExtraction.py
+++ extraction/predicateExtraction.py
@@ -18,4 +18,43 @@ import random
 # OPEN AI CLIENT
 load_dotenv()
 key = os.getenv("OPENAI_API_KEY")
-# openai_client = OpenAI(api_key = )
+openai_client = OpenAI(api_key = key)
+
+
+# FUNCTIONS --------------------------------
+
+def simplifyCondition(condition : str, context : str):
+  # condition - string
+  # context - string
+  response = openai_client.chat.completions.create(
+  model="gpt-4.1",
+  messages=
+    [
+      {
+          "role": "system",
+          "content": f'''
+                        You are given a condition. Context is provided, ONLY for resolving ambiguities. Simplify condition.
+                        Simplifications are then turned to logical expressions, want it to be easy to do.
+                        Condition may have unnecessary information. Remove it. (E.g. "If a match is found, a detection log is generated for auditing or alerting purposes", remove "for auditing or alerting purposes"; only care that it's generated.)
+                        Condition may have examples. Remove it. (E.g. "A predefined response is triggered if verification fails, such as disabling devices or blocking operations", remove "such as disabling...")
+                        If anything else is unnecessary, remove it or simplify it.
+                        Do not change meaning of condition.
+                        Only removals are substitutions allowed; no adding words.
+                        Use reasoning, and on a new line write the final answer. For example, you would return:
+                        <Reasoning>
+                        FINAL ANSWER:
+                        <simplified condition>.
+                        '''
+      },
+      {
+          "role": "user",
+          "content": "condition: " + condition + "\ncontext: " + context
+      }
+    ]
+  )
+  return  response.choices[0].message.content
+
+
+
+# ---
+
