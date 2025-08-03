c5f43513d395119055df6be08b0d32fdb7733c92
imjuliayue <juliayue6@gmail.com>
2025-08-02 20:24:21 -0400

    Starting Alloy Java script

---
+++ alloy/alloy.java
@@ -0,0 +1,9 @@
+// package Alloy;
+import edu.mit.csail.sdg.alloy4.Err;
+import edu.mit.csail.sdg.parser.CompUtil;
+import edu.mit.csail.sdg.ast.*;
+import edu.mit.csail.sdg.translator.*;
+
+public class alloy {
+    
+}
