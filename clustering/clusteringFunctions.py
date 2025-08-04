0cd60a4a5477f4c1ab275be0afd29574ae9c9e4f
imjuliayue <juliayue6@gmail.com>
2025-08-04 18:24:42 -0400

    Added load and save csv functions to Clustering

--- clustering/clusteringFunctions.py
+++ clustering/clusteringFunctions.py
@@ -53,6 +53,28 @@ def getEmbeds(values):
 
 # ---
 
+# ---
+
+def saveData(path, data, name):
+  # path is the path to save the data to (can include '../'), W.R.T. WHERE RUNNING SCRIPT
+  # data is in the form of [[name, descr1, descr2, ...], [name2, descr1, descr2, ...], ...]
+  # saves as CSV file with name as the first column and descriptions as the rest
+  with open(f'{path}/{name}.csv', 'w', newline='') as f:
+      writer = csv.writer(f)
+      writer.writerows(data)
+
+# ---
+
+def loadData(path, name):
+  # path is the path to load the data from (can include '../'), W.R.T. WHERE RUNNING SCRIPT
+  # name is the name of the file to load
+  # returns a list of lists, each list is [name, descr1, descr2, ...]
+  with open(f'{path}/{name}.csv', 'r') as f:
+      reader = csv.reader(f)
+      return [row for row in reader]
+  
+# ---
+
 def cleanForCluster():
    # NOTE: function adds another column to processed csv --> removes variables from predicate descriptions.
    pass
