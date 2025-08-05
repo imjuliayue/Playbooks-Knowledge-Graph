e2c4cad96b64fa7475cfbe0a61dad223c8149943
imjuliayue <juliayue6@gmail.com>
2025-08-05 12:58:53 -0400

    Fixed clustering file, updated pickle

--- clustering/DI-R50_Clustering.py
+++ clustering/DI-R50_Clustering.py
@@ -1,21 +1,47 @@
 from clusteringFunctions import *
 
+# FLAGS AND INITIAL VALUES -----------------
 FOLDERNAME = "DI-R50_Data"
+EMBED_FLAG = False
 
+# LOAD PICKLES ----------------------------
+# [condID, condDescr, techDescr], log expr, [preds], [vars], [predDescrs]
 postDI = loadPkl(FOLDERNAME, "DI-R50_Post_Unique")
 
 preDI = loadPkl(FOLDERNAME, "DI-R50_Pre_Unique")
 
+# CLEAN PREDICATE DESCRIPTIONS -----------
 cleanForCluster(postDI)
 cleanForCluster(preDI)
 # Now each of them have 5th is cleaned description
 
+# EMBED ----------------------------------
+# predicate names
+postPredNames = [item for cond in postDI for item in cond[2]]
+prePredNames = [item for cond in preDI for item in cond[2]]
 
-embedsPost = getEmbeds([item for cond in postDI for item in cond[5]])
-embedsPre = getEmbeds([item for cond in preDI for item in cond[5]])
+print(postPredNames[0:5])
 
-np.savetxt('PostDI-R50_embeddings.txt', embedsPost)
-np.savetxt('PreDI-R50_embeddings.txt', embedsPre)
+# predicate clean descriptions
+postPredCDescrs = [item for cond in postDI for item in cond[5]]
+prePredCDescrs = [item for cond in preDI for item in cond[5]]
+
+print(postPredCDescrs[0:5])
+
+# predicate uncleaned descriptions
+postPredDescrs = [item for cond in postDI for item in cond[4]]
+prePredDescrs = [item for cond in preDI for item in cond[4]]
+
+print(postPredDescrs[0:5])
+
+if EMBED_FLAG:
+    embedsPost = getEmbeds([item for cond in postDI for item in cond[5]])
+    embedsPre = getEmbeds([item for cond in preDI for item in cond[5]])
+    np.savetxt('data/DI-R50_Data/PostDI-R50_embeddings.txt', embedsPost)
+    np.savetxt('data/DI-R50_Data/PreDI-R50_embeddings.txt', embedsPre)
+else:
+    embedsPost = np.loadtxt("data/DI-R50_Data/PostDI-R50_embeddings.txt")
+    embedsPre = np.loadtxt("data/DI-R50_Data/PreDI-R50_embeddings.txt")
 
 # Change everything to normal floats
 testPost = embedsPost.tolist()
@@ -24,9 +50,11 @@ testPre = embedsPre.tolist()
 testPost = [[float(x) for x in arr] for arr in testPost]
 testPre = [[float(x) for x in arr] for arr in testPre]
 
-for cond, emb in zip(postDI + preDI, testPost + testPre):
-    cond.append(emb)
+all = []
 
-savePkl(FOLDERNAME, "DI-R50_CLUSTER_READY_ALL", postDI + preDI)
+for x in zip(postPredNames + prePredNames, postPredDescrs + prePredDescrs, postPredCDescrs + prePredCDescrs, testPost + testPre):
+    all.append(list(x))
+
+savePkl(FOLDERNAME, "DI-R50_CLUSTER_READY_ALL", all)
 
 
