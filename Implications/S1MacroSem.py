dde61f51fa19c124bea70bba21f81a12d138b945
imjuliayue <juliayue6@gmail.com>
2025-08-06 18:15:31 -0400

    Finished step 1 of implications (macro implications). Added embedding functions and cosine-similarity'd them, and decided on a threshold to use

---
+++ Implications/S1MacroSem.py
@@ -0,0 +1,50 @@
+from implicationFunctions import *
+from sklearn.metrics.pairwise import cosine_similarity
+
+FOLDERNAME = "DI-R50_Data/Implications"
+EMBEDFLAG = False
+
+# indices of D50 in full arrays.
+Preinds = np.array([496, 189, 462, 346, 426, 353, 406, 484, 399, 407, 284, 372, 387, 480, 255, 190, 425, 350, 398, 432, 471, 376, 302, 201, 458, 345, 305, 183, 279, 457, 388, 269, 300, 420, 224, 355, 296, 447, 499, 465, 205, 414, 321, 297, 476, 437, 368, 498, 210, 287])
+Postinds = np.array([123, 145, 287, 144, 124, 248, 247, 298, 317, 207, 242, 217, 319, 201, 288, 196, 150, 220, 153, 164, 294, 154, 336, 262, 184, 301, 191, 245, 161, 379, 252, 321, 358, 290, 141, 147, 194, 324, 125, 188, 282, 186, 344, 228, 223, 179, 255, 235, 175, 185])
+
+valuesPostcondition = pd.read_csv('data/PostconditionsWTechDescr.csv', header=None).to_numpy()
+valuesPrecondition = pd.read_csv('data/PreconditionsWTechDescr.csv', header=None).to_numpy()
+
+# Randomly selected 50 postconditions and 50 preconditions
+PostDI = valuesPostcondition[Postinds]
+PreDI = valuesPrecondition[Preinds]
+
+# Embed the postconditions and preconditions
+
+if EMBEDFLAG: 
+    embPost, costPost = getEmbeds(PostDI[:,1])
+    embPre, costPre = getEmbeds(PreDI[:,1])
+
+    print(f"Cost: {(costPost + costPre) / 130000}")
+
+    np.savetxt(f"data/{FOLDERNAME}/R50PostCondEmbs.txt", embPost)
+    np.savetxt(f"data/{FOLDERNAME}/R50PreCondEmbs.txt", embPre)
+else:
+    embPre = np.loadtxt(f"data/{FOLDERNAME}/R50PreCondEmbs.txt")
+    embPost = np.loadtxt(f"data/{FOLDERNAME}/R50PostCondEmbs.txt")
+
+# create cos matrix
+CosMatrix = cosine_similarity(embPost,embPre) #rows = post, cols = pre
+print(np.min(CosMatrix))
+
+# no normalizing!
+
+# threshold
+thresh = 0.272
+CosMatrix[CosMatrix < thresh] = 0
+
+# Save CSV
+labelsRow = PostDI[:,1]
+labelsCol = PreDI[:,1]
+
+
+df = pd.DataFrame(CosMatrix, index=labelsRow, columns=labelsCol)
+
+df.to_csv(f"CosSim{thresh}.csv")
+
--- Implications/implicationFunctions.py
+++ Implications/implicationFunctions.py
@@ -3,10 +3,15 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
+import pandas as pd
+
 import numpy as np
+
+import json
 
 import csv
 
+from tenacity import retry, wait_random_exponential, stop_after_attempt
 import pickle as pkl
 
 import ast
@@ -26,3 +31,107 @@ def openai_client():
     return _openai_client
 
 # ---
+
+def predImplication(axiom, pred):
+  client = openai_client()
+  response = client.chat.completions.create(
+  model="gpt-4.1",
+  messages=
+    [
+      {
+          "role": "system",
+          "content": f'''
+                        You are given a list of axiom predicates and one theorem predicate.
+                        Determine whether the axiom predicates imply the theorem predicate.
+                        Include reasoning, and in the last line, write FINAL ANSWER: and include your final answer as either "True" or "False", like this:
+                        FINAL ANSWER:
+                        Output "True" or "False"
+                        '''
+      },
+      {
+          "role": "user",
+          "content": "axioms: " + axiom + "\ntheorem: " + pred
+      }
+    ]
+  )
+
+  return  response.choices[0].message.content, response.usage[0], response.usage[1]
+
+# FUNCTIONS --------------------------------
+
+# OPEN AI CLIENT
+load_dotenv()
+_openai_client = None
+
+def openai_client():
+    global _openai_client
+    if _openai_client is None:
+        _openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    return _openai_client
+
+# ---
+
+def get_embed(text):
+  emb = openai_client().embeddings.create(input = [text], model="text-embedding-3-large")
+  return emb.data[0].embedding, emb.usage.prompt_tokens
+
+# ---
+
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+def getEmbeds(values):
+  embeds = []
+  totalToks=0
+  i = 0
+  for item in values:
+    embed,tokens = get_embed(item)
+    totalToks+=tokens
+    # if(i % 500 == 0):
+    print(i)
+    i+=1
+    embeds.append(embed)
+  return np.array(embeds), totalToks
+
+# ---
+
+def saveData(path, name, data):
+  # path is the path to save the data to (can include '../'), W.R.T. WHERE RUNNING SCRIPT
+  # data is in the form of [[name, descr1, descr2, ...], [name2, descr1, descr2, ...], ...]
+  # saves as CSV file with name as the first column and descriptions as the rest
+  with open(f'{path}/{name}.csv', 'w', newline='') as f:
+      writer = csv.writer(f)
+      writer.writerows(data)
+
+# ---
+
+def loadData(path, name):
+  # path is the path to load the data from (can include '../'), W.R.T. WHERE RUNNING SCRIPT
+  # name is the name of the file to load
+  # returns a list of lists, each list is [name, descr1, descr2, ...]
+  with open(f'{path}/{name}.csv', 'r') as f:
+      reader = csv.reader(f)
+      return [row for row in reader]
+
+# ---
+
+# saves 1d array to txt file
+def saveTxt(FOLDERNAME, FILENAME, ARRAY):
+    # PATHWAY IS W.R.T. WHERE RUNNING SCRIPT
+    with open(f"data/{FOLDERNAME}/{FILENAME}.json", 'w') as f:
+        json.dump(ARRAY, f)
+    # np.savetxt(f'data/{FOLDERNAME}/{FILENAME}', np.array(ARRAY).reshape((-1,1)),fmt="%s")
+
+def loadTxt(FOLDERNAME, FILENAME):
+    # PATHWAY IS W.R.T. WHERE RUNNING SCRIPT
+    with open(f"data/{FOLDERNAME}/{FILENAME}.json", 'r') as f:
+        return json.load(f)
+    
+def savePkl(FOLDERNAME, FILENAME, DATA):
+    # PATHWAY IS W.R.T. WHERE RUNNING SCRIPT
+    with open(f"data/{FOLDERNAME}/{FILENAME}.pkl", 'wb') as f:
+        pkl.dump(DATA, f)
+
+def loadPkl(FOLDERNAME, FILENAME):
+    # PATHWAY IS W.R.T. WHERE RUNNING SCRIPT
+    with open(f"data/{FOLDERNAME}/{FILENAME}.pkl", 'rb') as f:
+        return pkl.load(f)
+    
