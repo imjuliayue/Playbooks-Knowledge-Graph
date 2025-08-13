f2c1a53249c7415dc087e0d6f2673bd84afb92a1
Jonas <jonass@andrew.cmu.edu>
2025-08-12 20:21:37 -0400

    added function to get dictionary from cluster_name to cluster_descr and added csv with all unique cluster names and descr

--- clustering/test.py
+++ clustering/test.py
@@ -20,10 +20,14 @@ names, descr, variables, sim_matrix = get_pred_list(test)
 # clusters = clustering_clique_method(sim_matrix, names, variables, 0.825)
 # output_as_csv(clusters, names, descr, variables, 0.825)
 
-path = "clustering_results"
-name = "clustering_after_chatGPT_naming"
-test_dict = create_cluster_dictionary(path, name)
-path_to_unique = "../data/DI-R50_Data/Extraction"
-save_file_path = "../data/replacement_test"
-replace_pred_with_cluster_names(path_to_unique, "DI-R50_Post_Unique", test_dict, save_file_path, "DI-R50_Post")
-replace_pred_with_cluster_names(path_to_unique, "DI-R50_Pre_Unique", test_dict, save_file_path, "DI-R50_Pre")
+# path = "clustering_results"
+# name = "clustering_after_chatGPT_naming"
+# test_dict = create_cluster_dictionary(path, name)
+# path_to_unique = "../data/DI-R50_Data/Extraction"
+# save_file_path = "../data/replacement_test"
+# replace_pred_with_cluster_names(path_to_unique, "DI-R50_Post_Unique", test_dict, save_file_path, "DI-R50_Post")
+# replace_pred_with_cluster_names(path_to_unique, "DI-R50_Pre_Unique", test_dict, save_file_path, "DI-R50_Pre")
+
+cluster_dict, cluster_names_list, cluster_descr_list = get_unique_clusters("../clustering/clustering_results", "clustering_after_chatGPT_naming")
+# print(cluster_names_list)
+output_unique_clusters_to_csv(cluster_names_list, cluster_descr_list, "unique_cluster_name_and_descr")
